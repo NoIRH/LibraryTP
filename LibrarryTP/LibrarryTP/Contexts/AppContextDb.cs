7470b8b047d3f49ed89a1f65db5c0ee41a890f57
Victor <67371337+Gveideon@users.noreply.github.com>
2023-10-11 13:32:13 +0300

    add models and db controller

---
+++ LibrarryTP/LibrarryTP/Contexts/AppContextDb.cs
@@ -0,0 +1,21 @@
+﻿using LibrarryTP.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarryTP.Contexts
+{
+    internal class AppContextDb : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+        public string Path { get; set; } = "Data Source = homebank.db";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Path);
+        }
+    }
+}
---
+++ LibrarryTP/LibrarryTP/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarryTP.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SecondName { get; set; }
+
+        public DateTime DateBirth { get; set; }
+        public int Role { get; set; }
+    }
+}
